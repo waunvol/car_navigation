63419888ecf84a07265f10b6188a6fd6a390ef85
heho <943241156@qq.com>
2023-01-01 15:41:47 +0800

    Update ctrl node.

--- navigation/car_motion_ctrl/src/ctrl_node.cpp
+++ navigation/car_motion_ctrl/src/ctrl_node.cpp
@@ -5,8 +5,7 @@
 
 /**
  * to do:
- * 1. inverse hybirdstar path
- * 2. fix astar path
+ * a more generic controller base
  */
 
 std::atomic<bool> rec_flag;
@@ -46,12 +45,17 @@ void UpdatePose(const geometry_msgs::Pose msg) {
 
 int main(int argc, char** argv)
 {
+    // init
     ros::init(argc, argv, "ctrl_node");
     ros::NodeHandle n;
 
     rec_flag = 0;
-    string PathName;// = argv[1]; 
-    PathName = "Dijkstra_path";
+    string PathName = "";   //
+    string controller_type = "";
+    if (not n.getParam("selected_path", PathName))
+        PathName = "Dijkstra_path";
+    if (not n.getParam("controller_type", controller_type))
+        controller_type = "pid";
     int frequency = 20;
 
     std::vector<Pose_t> global_path;
@@ -71,11 +75,12 @@ int main(int argc, char** argv)
     controller.InitController(0.05, frequency);
     controller.SetLinearPID(0.04, 0.005, 0);
     controller.SetAngularPID(0.08, 0.001, 0);
-    std::pair<double, double> cur_speed = {0.0, 0.0};
-
+    
     DWA_planner local_planner;
 
     // speed set to zero
+    std::pair<double, double> cur_speed = {0.0, 0.0};
+
     geometry_msgs::Twist speed;
     speed.angular.z = 0;
     speed.linear.x = 0;
@@ -84,19 +89,23 @@ int main(int argc, char** argv)
     spinner.start();
     while(ros::ok())
     {
-
+        ROS_INFO_THROTTLE(5.0, "current navigation path: %s, current controller: %s", PathName.c_str(), controller_type.c_str());
         if(rec_flag.load(std::memory_order_relaxed))
         {
-            path_lock.lock();   
             rec_flag.store(false, std::memory_order_relaxed);
-            local_planner.UpdatePlanning(global_path);
-            path_lock.unlock();
-
-            // path_lock.lock();
-            // // controller.setGlobalPath(global_path);
-            // global_path.clear();
-            // path_lock.unlock();
-
+            if (controller_type == "dwa")
+            {
+                path_lock.lock();   
+                local_planner.UpdatePlanning(global_path);
+                path_lock.unlock();
+            }
+            else if (controller_type == "pid")
+            {
+                path_lock.lock();
+                controller.setGlobalPath(global_path);
+                global_path.clear();
+                path_lock.unlock();
+            }
             while(!rec_flag.load(std::memory_order_relaxed)) // interrupt when get new goal;
             {
                 if (rec_flag.load(std::memory_order_relaxed))
@@ -111,13 +120,17 @@ int main(int argc, char** argv)
                     ROS_INFO("Navigation finish!");
                     break;
                 }
-                // cur_speed = controller.CalculateValue(cur_pose, cur_speed.first, cur_speed.second);
-                local_planner.CalculateSpeed(cur_pose, cur_speed);
+                if (controller_type == "pid")
+                    cur_speed = controller.CalculateValue(cur_pose, cur_speed.first, cur_speed.second);
+                else if (controller_type == "dwa")
+                {
+                    local_planner.CalculateSpeed(cur_pose, cur_speed);
+                    local_path_pub.publish(local_planner.GetLastTrajectory());
+                }
                 speed.linear.x = cur_speed.first;
                 speed.angular.z = cur_speed.second;
-                ROS_INFO_THROTTLE(1.0, "Robot current speed: angular %f, linear %f", cur_speed.second, cur_speed.first);
+                ROS_INFO_THROTTLE(2.0, "Robot current speed: angular %f, linear %f", cur_speed.second, cur_speed.first);
                 speed_pub.publish(speed);
-                local_path_pub.publish(local_planner.GetLastTrajectory());
                 ros::spinOnce();
                 r.sleep();
             }
